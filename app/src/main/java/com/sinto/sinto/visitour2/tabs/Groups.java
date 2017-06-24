558dfd3c88d9a23efd83f3ece6dcf8e97362e027
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2017-06-25 07:50:58 +0800

    Add map implementation

--- app/src/main/java/com/sinto/sinto/visitour2/tabs/Groups.java
+++ app/src/main/java/com/sinto/sinto/visitour2/tabs/Groups.java
@@ -1,12 +1,16 @@
 package com.sinto.sinto.visitour2.tabs;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.sinto.sinto.visitour2.MapsActivity;
 import com.sinto.sinto.visitour2.R;
+import com.sinto.sinto.visitour2.TabActivity;
 
 /**
  * Created by Arvin on 6/25/2017.
@@ -16,7 +20,19 @@ public class Groups extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+
         View rootView = inflater.inflate(R.layout.fragment_groups, container, false);
+
+        // Assign fields
+        Button mapButton = (Button) rootView.findViewById(R.id.map_button);
+        // Set click listeners
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MapsActivity.class));
+            }
+        });
+
         return rootView;
     }
 }
