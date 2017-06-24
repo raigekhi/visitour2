7ce51e81bd2d72f1a522354cadbf69988487e1de
Nathaniel M. Capule <llss.s.sls.lll@gmail.com>
2017-06-25 06:21:09 +0800

    Initial commit

---
+++ app/src/main/java/com/sinto/sinto/visitour2/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.sinto.sinto.visitour2;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.view.WindowManager;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class SplashActivity extends AwesomeSplash {
+
+    @Override
+    public void initSplash(ConfigSplash configSplash)
+    {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Background Animation
+        configSplash.setBackgroundColor(R.color.bg_color);
+        configSplash.setAnimCircularRevealDuration(3000);
+        configSplash.setRevealFlagX(Flags.WITH_LOGO);
+
+        //Logo
+        configSplash.setLogoSplash(R.drawable.loogo);
+        configSplash.setAnimLogoSplashDuration(5000);
+        configSplash.setAnimLogoSplashTechnique(Techniques.Flash);
+
+
+        //Title
+        configSplash.setTitleSplash("");
+
+
+    }
+
+    @Override
+    public void animationsFinished() {
+        startActivity(new Intent(SplashActivity.this,SignInActivity.class));
+    }
+}
---
+++ app/src/main/java/com/sinto/sinto/visitour2/tabs/Calendar.java
@@ -0,0 +1,22 @@
+package com.sinto.sinto.visitour2.tabs;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sinto.sinto.visitour2.R;
+
+/**
+ * Created by Arvin on 6/25/2017.
+ */
+
+public class Calendar extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_calendar, container, false);
+        return rootView;
+    }
+}
